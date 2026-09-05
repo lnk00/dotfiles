25afe5c8c68413dc00d7a3cab8be53e0d7b2e6bd
Damien Dumontet <damien@fullenrich.com>
2026-09-04 22:59:34 -0300

    remove rounded border diagnostic window

--- nvim/init.lua
+++ nvim/init.lua
@@ -120,7 +120,7 @@ do
 	vim.diagnostic.config({
 		update_in_insert = false,
 		severity_sort = true,
-		float = { border = "rounded", source = "if_many" },
+		float = { source = "if_many" },
 		underline = { severity = { min = vim.diagnostic.severity.WARN } },
 
 		-- Can switch between these as you prefer
